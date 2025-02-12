2bbebb8dc6d24766214af3dbf60812ddacfc77c8
GustavoDoosAnjos <anjos.gu@gmail.com>
2025-02-12 18:38:32 -0300

    fix: Adiciona comentário

--- src/models/Tabuleiro.ts
+++ src/models/Tabuleiro.ts
@@ -4,10 +4,12 @@ export class Tabuleiro {
   propriedades: Propriedade[];
   tamanho = 20;
 
+  // Como não estava especificado os preços de aluguel e compra eu deixei aleatório.
+
   constructor() {
     this.propriedades = Array.from(
       { length: this.tamanho },
-      (_, i) =>
+      (_) =>
         new Propriedade(
           Math.floor(Math.random() * 200),
           Math.floor(Math.random() * 70)
